1c1227d6a76525a53ea834c322846b3c5a554b39
RubenWWW <145838373+RubenWWW@users.noreply.github.com>
2024-03-04 00:30:12 +0100

    Add files via upload

---
+++ primero.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+sudo apt install apache2 git binutils -y
+cd /home/ubuntu
+sudo git clone https://github.com/aws/efs-utils
+sudo git clone https://github.com/RubenWWW/aws-cf-web
+cd ./efs-utils/
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install ./build/amazon-efs-utils*.deb -y
+sudo mkdir -p /var/www/html/efs
+sudo mount -t efs fs-0bf28a27f5898b2ed:/ /var/www/html/efs
+sudo rm /etc/apache2/sites-enabled/000-default.conf
+cd /home/ruben/aws-cf-web/
+sudo mv ./cosas.conf /etc/apache2/sites-enabled/000-default.conf
+sudo systemctl reload apache2
+
+  
