232c59e2c55578cdde78e44c53011e5435707152
RubenWWW <145838373+RubenWWW@users.noreply.github.com>
2024-03-04 16:57:32 +0100

    Update primero.sh

--- primero.sh
+++ primero.sh
@@ -3,11 +3,11 @@ sudo apt install apache2 git binutils -y
 cd /home/ubuntu
 sudo git clone https://github.com/aws/efs-utils
 sudo git clone https://github.com/RubenWWW/aws-cf-web
-cd ./efs-utils/
-sudo chmod -x ./build-deb.sh
-sudo ./build-deb.sh
+cd /home/ubuntu/efs-utils/
+sudo chmod -x /home/ubuntu/efs-utils/build-deb.sh
+sudo ./home/ubuntu/efs-utils/build-deb.sh
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install ./build/amazon-efs-utils*.deb -y
+sudo apt install /home/ubuntu/efs-utils/build/amazon-efs-utils*.deb -y
 sudo mkdir -p /var/www/html/efs
 sudo mount -t efs fs-0bf28a27f5898b2ed:/ /var/www/html/efs
 sudo rm /etc/apache2/sites-enabled/000-default.conf
