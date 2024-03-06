0d0e58342e2316568f1ceed55b13cc78368ad65f
RubenWWW <145838373+RubenWWW@users.noreply.github.com>
2024-03-06 12:02:27 +0100

    Add files via upload

---
+++ haproxy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+sudo apt update -y
+sudo apt install git binutils haproxy -y
+cd /home/ubuntu
+sudo git clone https://github.com/RubenWWW/aws-cf-web
+cd ./aws-cf-web
+sudo cp ./haproxy.cfg /etc/haproxy/haproxy.cfg
+sudo systemctl start haproxy
+
+
