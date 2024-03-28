b2f780e75f8477c05a62e4f0e5cc1d9a96d9526a
RubenWWW <145838373+RubenWWW@users.noreply.github.com>
2024-03-28 20:02:49 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -6,8 +6,16 @@ DB_USER="rbn"
 DB_PASSWORD="informatica"
 
 sudo apt-get update
-sudo apt-get install -y mysql-server php php-mysql curl
+sudo apt-get install -y mysql-server git php php-mysql curl
 
+sudo git clone https://github.com/aws/efs-utils
+cd /home/ubuntu/efs-utils/
+sudo chmod +x /home/ubuntu/efs-utils/build-deb.sh
+sudo ./build-deb.sh
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install /home/ubuntu/efs-utils/build/amazon-efs-utils*.deb -y
+sudo mkdir -p /var/www/html/efs
+sudo mount -t efs fs-0bf28a27f5898b2ed:/ /var/www/html/efs
 sudo mkdir -p /var/www/html/efs/wordpress
 
 sudo mysql <<MYSQL_SCRIPT
